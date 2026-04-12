bc0f9b9d2cc0b52f4bf5039b3c71a0566bfb5fdb
Alex <alex@monei.com>
2026-04-12 08:02:39 +0000

    chore: add CI configs, Dependabot, ESLint, Jest, CODEOWNERS
    - Dependabot: weekly npm updates for processor + enabler + GH Actions
    - CI: improved workflow with connect.yaml validation job
    - ESLint: TypeScript config for both modules
    - Jest: ts-jest configs (node env for processor, jsdom for enabler)
    - CODEOWNERS: jimmyn as default reviewer
    - Prettier: shared config
    - Updated devDependencies with @typescript-eslint packages

---
+++ enabler/jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.html',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+};
---
+++ processor/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/scripts/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+};
